79c6cd025a13c360a488933a1913ee4ac663dea3
Ygor BarbosaHays <Ygor.BarbosaHays@modec.com>
2022-06-17 15:27:54 -0300

    chore: wip

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,14 +1,19 @@
 import { ApolloServer } from "apollo-server-micro";
-import { typeDefs } from "../../graphql/schema";
+import { schema } from "../../graphql/schema";
 import { resolvers } from "../../graphql/resolvers";
+import { createContext } from "../../graphql/context";
 import Cors from "micro-cors";
 
 const cors = Cors();
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({ 
+  schema, 
+  resolvers,
+  context: createContext,
+});
 const startServer = apolloServer.start();
 
 export default cors(async function handler(req,res) {
-  if(req.mehtod === "OPTIONS") {
+  if(req.method === "OPTIONS") {
     res.end()
     return false
   }
